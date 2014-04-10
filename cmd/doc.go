a1e946cf6309da4e411d2785e8d0110b03aae810
Petar Maymounkov <petarm@gmail.com>
2014-04-10 17:37:19 -0400

    tidyup

--- cmd/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2013 Tumblr, Inc.
-// Use of this source code is governed by the license for
-// The Go Circuit Project, found in the LICENSE file.
-//
-// Authors:
-//   2013 Petar Maymounkov <p@gocircuit.org>
-
-// Package cmd encloses command-line executables
-package doc
