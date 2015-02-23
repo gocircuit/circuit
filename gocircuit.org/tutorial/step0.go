2087342001dfdff5debf549ba60adda7f5c35561
Petar Maymounkov <petarm@gmail.com>
2015-02-22 20:54:48 -0500

    add ec2 scripts

--- gocircuit.org/tutorial/step0.go
+++ gocircuit.org/tutorial/step0.go
@@ -44,7 +44,33 @@ const installBody = `
 
 <p>Next, configure the system to start the circuit daemon during the system booting sequence.
 
-<p>??
+<p>To keep things as simple as possible, we start the circuit from <code>/etc/rc.local</code>.
+
+<pre>
+	# mkdir /var/circuit
+</pre>
+
+<p>Add the following shell script <code>/usr/local/bin/start-first-server.sh</code>:
+
+<pre>
+	#!/bin/sh
+	# Save the EC2 private IP address of this host to a variable.
+	ip_address=` + "`" + `ifconfig eth0 | awk '/inet addr/ {split($2, a, ":"); print a[2] }'` + "`" + `
+	# Start the circuit server
+	/usr/local/bin/circuit start -a ${ip_address}:11022 1> /var/circuit/address 2> /var/circuit/log &
+</pre>
+
+<p>Add the following shell script <code>/usr/local/bin/start-joining-server.sh</code>:
+
+<pre>
+	#!/bin/sh
+	# Save the EC2 private IP address of this host to a variable.
+	ip_address=` + "`" + `ifconfig eth0 | awk '/inet addr/ {split($2, a, ":"); print a[2] }'` + "`" + `
+	# Start the circuit server
+	/usr/local/bin/circuit start -a ${ip_address}:11022 -j $1 1> /var/circuit/address 2> /var/circuit/log &
+</pre>
+
+
 
 <h3>Install MySQL server</h3>
 
---
+++ tutorial/ec2/start-first-circuit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Save the EC2 private IP address of this host to a variable.
+ip_address=` + "`" + `ifconfig eth0 | awk '/inet addr/ {split($2, a, ":"); print a[2] }'` + "`" + `
+
+# Start the circuit server
+/usr/local/bin/circuit start -a ${ip_address}:11022 1> /var/circuit/address 2> /var/circuit/log &
