b5f8a5e43e35b33735038525a4dbed65f8d732d7
Petar Maymounkov <petarm@gmail.com>
2015-02-25 20:51:24 -0500

    tuneups

--- tutorial/ec2/start-joining-circuit.sh
+++ tutorial/ec2/start-joining-circuit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Save the EC2 private IP address of this host to a variable.
-ip_address=`ifconfig eth0 | awk '/inet addr/ {split($2, a, ":"); print a[2] }'`
+ip_address=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 # Start the circuit server
 /usr/local/bin/circuit start -a ${ip_address}:11022 -j $1 1> /var/circuit/address 2> /var/circuit/log &
