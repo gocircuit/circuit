80597ae8cde06ab894beb6b3cab8f48c230482aa
Petar Maymounkov <petarm@gmail.com>
2015-03-16 18:37:52 -0400

    complete name server element doc

--- gocircuit.org/api/name.go
+++ gocircuit.org/api/name.go
@@ -58,21 +58,29 @@ a DNS resource record in standard DNS notation. The syntax of these records are 
 in the documentation of <a href="http://github.com/miekg/dns">github.com/miekg/dns</a>
 as well as this <a href="http://miek.nl/posts/2014/Aug/16/go-dns-package/">related blog article</a>.
 
-<p>??
+<p>Every DNS resource record, for instance <code>"miek.nl. 3600 IN MX 10 mx.miek.nl."</code>,
+starts with the name that the record pertains to. Each invocation of the <code>Set</code> command
+<em>adds</em> a record to the list of records pertaining to the respective name.
+For instance, the following command adds the record <code>"miek.nl. 3600 IN MX 10 mx.miek.nl."</code>
+to the name <code>"miek.nl."</code>
 <pre>
 	if err := ns.Set("miek.nl. 3600 IN MX 10 mx.miek.nl."); err != nil {
 		… // DNS record cannot be recognized
 	}
 </pre>
 
-<p>??
+<p>The <code>Unset</code> method removes <em>all</em> records associated with a given name. For instance,
 <pre>
 	ns.Unset("miek.nl.")
 </pre>
 
 <h3>Server status</h3>
 
-<p>??
+<p>At any point, the user can asynchronously retrieve the current status of a name server element, 
+using the <code>Peek</code> method of the element. The returned structure (shown below)
+contains a textual representation of the DNS server's address, and a map of all names and their
+associated lists of resource records.
+
 <pre>
 	type NameserverStat struct {
 		Address string
