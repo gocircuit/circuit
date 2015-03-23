0564a43d8c52b6016f2ffce3eeee73704f736ca3
Petar Maymounkov <petarm@gmail.com>
2015-03-23 08:36:10 -0400

    complete channel api doc

--- gocircuit.org/api/channel.go
+++ gocircuit.org/api/channel.go
@@ -29,6 +29,14 @@ buffer, until the next send operation is performed.
 	MakeChan(n int) (Chan, error)
 </pre>
 
+<p>For instance,
+<pre>
+	ch, err := anchor.MakeChan(0)
+	if err != nil {
+		… // this anchor is already busy
+	}
+</pre>
+
 <p>An application error will be returned only if the anchor already has an
 element attached to it. The integer parameter specifies the number of
 messages the channel will buffer. This must be a non-negative integer.
@@ -49,13 +57,50 @@ channel element, even if messages are pending in the channel's buffer.
 
 <h3>Sending and receiving messages</h3>
 
-<p>??
+<p>The send/receive mechanism transports <code>io.ReadWriteCloser</code>
+pipes between the sender and the receiver. The sender gets the write end
+and the receiver gets the read end of the pipe.
+
+<p>To send a message, the user invokes <code>Send</code>. If the channel
+has been closed or aborted, a non-nil application error is returned. Otherwise,
+a new pipe is created. Send attempts to send the pipe itself through the
+channel. It will block until there is space in the channel buffer or there is
+a matching call to <code>Receive</code>.
+
+<p>When <code>Send</code> unblocks it returns an <code>io.WriteCloser</code> wherein
+the user writes the content of the message and closes the stream to indicate end
+of message.
+<pre>
+	w, err := ch.Send()
+	if err != nil {
+		… // channel has been closed
+	}
+	w.Write(msg)
+	w.Close()
+</pre>
+
+<p>To receive a message, the user invokes <code>Recv</code>. The operation blocks
+until there is a pipe in the channel buffer or a matching call to <code>Send</code>. 
+Receive will return an application error if the channel has been closed. Otherwise
+it returns the reader end of the pipe received.
 
 <h3>Channel closure</h3>
 
-<p>??
+<p>Channels can be closed synchronously with the <code>Close</code> method.
+The closure event will be communicated to the receiver after all pending messages
+have been received. <code>Close</code> returns an application error only if the
+channel has already been clsoed.
+
+<p>An alternative method of closing the channel is to invoke <code>Scrub</code>.
+This will abort the channel and discard any pending messages.
 
 <h3>Retrieving channel state</h3>
+
+<p>The <code>Stat</code> method can be called asynchronously to retrieve the
+current state of the channel. The returned structure includes the channel capacity,
+whether the channel has been closed or aborted, the number of messages sent
+and received. (The difference of sent and received messages is the count of messages
+	pending reception in the channel buffer.)
 <pre>
 	type ChanStat struct {
 		Cap int
